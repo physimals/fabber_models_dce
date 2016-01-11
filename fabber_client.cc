5d26176408b77f9b1dc42fde0a4e587551c6fa21
Jesper Kallehauge <jesper.kallehauge@eng.ox.ac.uk>
2016-01-11 16:13:46 +0000

    Update client file with the ETM model

--- fabber_client.cc
+++ fabber_client.cc
@@ -9,32 +9,19 @@
 #include "fabbercore/fabber_core.h"
 
 // CEST models to be included from library
-#include "fwdmodel_dce.h"]
+#include "fwdmodel_dce.h"
+#include "fwdmodel_dce_ETM.h"
 #include "fwdmodel_dce_CTU.h"
 #include "fwdmodel_dce_2CXM.h"
 
 int main(int argc, char** argv) {
 
-  //add the CEST models - these will autoregister at this point
+  //add the DCE models - these will autoregister at this point
   DCEFwdModel::NewInstance();
+  DCE_ETM_FwdModel::NewInstance();
   DCE_CTU_FwdModel::NewInstance();
   DCE_2CXM_FwdModel::NewInstance();
   
   return execute(argc, argv);
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
