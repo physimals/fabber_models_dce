2d893b06f5a2f2819ee213c7f52f53001dfbb5be
Jesper Kallehauge <jesper.kallehauge@eng.ox.ac.uk>
2016-02-05 17:55:34 +0000

    initial commit

---
+++ fwdmodel_dce_ETM_LLS.h
@@ -0,0 +1,86 @@
+/*  fwdmodel_asl_grase.h - Implements the GRASE model
+
+    Michael Chappell, FMRIB Image Analysis Group
+
+    Copyright (C) 2007 University of Oxford  */
+
+/*  CCOPYRIGHT */
+
+#include "fabbercore/fwdmodel.h"
+#include "fabbercore/inference.h"
+#include <string>
+using namespace std;
+
+class DCE_ETM_LLS_FwdModel : public FwdModel {
+public:
+  static FwdModel* NewInstance();
+
+  // Virtual function overrides
+  virtual void Initialize(ArgsType& args);
+  virtual void Evaluate(const ColumnVector& params, 
+			      ColumnVector& result) const;
+  virtual vector<string> GetUsage() const;
+  virtual string ModelVersion() const;
+                  
+  virtual void DumpParameters(const ColumnVector& vec,
+                                const string& indents = "") const;
+                                
+  virtual void NameParams(vector<string>& names) const;     
+  virtual int NumParams() const 
+  { return 5 + (inferdelay?1:0); }
+
+  virtual ~DCE_ETM_LLS_FwdModel() { return; }
+
+  virtual void HardcodedInitialDists(MVNDist& prior, MVNDist& posterior) const;
+
+protected: 
+
+  ColumnVector aifshift( const ColumnVector& aif, const float delta, const float hdelt ) const;
+  void createconvmtx( LowerTriangularMatrix& A, const ColumnVector aifnew ) const;
+  
+// Constants
+
+  // Lookup the starting indices of the parameters
+  int Ktrans_index() const {return 1;}
+
+  int Ve_index() const {  return 2;  }
+
+  int Vp_index() const {  return 3;  }
+
+  int delta_index() const { return 3 + (inferdelay?1:0); }
+
+  int T10_index() const { return 4 + (inferdelay?1:0); }
+
+  int sig0_index() const { return 5 + (inferdelay?1:0); }
+
+  //for ARD
+  vector<int> ard_index;
+
+    // scan parameters
+  //double te;
+  //double r2;
+  double delt;
+  double TR;
+  double FA;
+  double r1;
+  double Tsat;
+  double FA_radians;
+
+  ColumnVector artsig;
+  ColumnVector s;
+
+  bool aifconc;
+
+  bool inferdelay;
+  bool doard;
+
+  bool imageprior;
+
+  string convmtx;
+  string Acq_tech;
+  
+  private:
+  /** Auto-register with forward model factory. */
+  static FactoryRegistration<FwdModelFactory, DCE_ETM_LLS_FwdModel> registration;
+
+};
