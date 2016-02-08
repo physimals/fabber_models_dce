885a3bc09e0222d4cff1a73885b81e86e31d5e46
Jesper Kallehauge <jesper.kallehauge@eng.ox.ac.uk>
2016-02-08 14:01:54 +0000

    compatible with DCE-CT data

--- fwdmodel_dce_2CXM.h
+++ fwdmodel_dce_2CXM.h
@@ -17,28 +17,28 @@ public:
 
   // Virtual function overrides
   virtual void Initialize(ArgsType& args);
-  virtual void Evaluate(const ColumnVector& params, 
-			      ColumnVector& result) const;
+  virtual void Evaluate(const ColumnVector& params,
+                  ColumnVector& result) const;
   virtual vector<string> GetUsage() const;
   virtual string ModelVersion() const;
-                  
+
   virtual void DumpParameters(const ColumnVector& vec,
                                 const string& indents = "") const;
-                                
-  virtual void NameParams(vector<string>& names) const;     
-  virtual int NumParams() const 
+
+  virtual void NameParams(vector<string>& names) const;
+  virtual int NumParams() const
   { return 6 + (inferdelay?1:0); }
 
   virtual ~DCE_2CXM_FwdModel() { return; }
 
   virtual void HardcodedInitialDists(MVNDist& prior, MVNDist& posterior) const;
 
-protected: 
+protected:
 
   ColumnVector aifshift( const ColumnVector& aif, const float delta, const float hdelt ) const;
   ColumnVector expConv( const ColumnVector& aifnew,  const float T, const ColumnVector htsamp) const;
   void createconvmtx( LowerTriangularMatrix& A, const ColumnVector aifnew ) const;
-  
+
 // Constants
 
   // Lookup the starting indices of the parameters
@@ -52,9 +52,11 @@ protected:
 
   int delta_index() const { return 4 + (inferdelay?1:0); }
 
-  int T10_index() const { return 5 + (inferdelay?1:0); }
+  int sig0_index() const { return 5 + (inferdelay?1:0); }
 
-  int sig0_index() const { return 6 + (inferdelay?1:0); }
+  int T10_index() const { return 6 + (inferdelay?1:0); }
+
+
 
   //for ARD
   vector<int> ard_index;
@@ -77,7 +79,7 @@ protected:
 
   string Acq_tech;
   string convmtx;
-  
+
   private:
   /** Auto-register with forward model factory. */
   static FactoryRegistration<FwdModelFactory, DCE_2CXM_FwdModel> registration;
