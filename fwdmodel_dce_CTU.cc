702e02049f933603fa1cfe32a7ee5d2598553b94
Jesper Kallehauge <jesper.kallehauge@eng.ox.ac.uk>
2016-02-11 15:22:28 +0000

    changed order of output parameters

--- fwdmodel_dce_CTU.cc
+++ fwdmodel_dce_CTU.cc
@@ -365,8 +365,8 @@ void DCE_CTU_FwdModel::NameParams(vector<string>& names) const
        if (Acq_tech == "CT") {
            names.push_back("sig0");
        }else{
-    names.push_back("T10");
     names.push_back("sig0");
+    names.push_back("T10");
        }
    }
 
